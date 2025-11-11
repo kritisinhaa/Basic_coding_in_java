71f676dd7977e1596264a392247667aeb0b34797
Kritisinha879 <132038131+Kritisinha879@users.noreply.github.com>
2025-11-11 17:26:02 +0530

    Initial commit

---
+++ Program1.java
@@ -0,0 +1,16 @@
+// Write a Java Program to print your name, date of birth, and mobile number. 
+class Program1{
+  
+  public static void main(String[] args){
+  String name = "Kriti Sinha";
+  String DOB ="13-sept-2002";
+  String Mobile ="8603780743";
+  
+    System.out.println("Name :" + name);
+    System.out.println("Dob :" + DOB);
+    System.out.println("Mobile :" + Mobile);
+
+
+  }
+
+}
---
+++ Program2.java
@@ -0,0 +1,12 @@
+public class Program2 {
+      public static void main(String [] args){
+        char ch1 = 'X';
+        char ch2 ='M';
+        char ch3 ='L';
+
+        System.out.println("the reverse of" + ch1 +ch2 +ch3 +" is " + ch3+ch2+ ch1);
+
+      }
+
+          
+}
